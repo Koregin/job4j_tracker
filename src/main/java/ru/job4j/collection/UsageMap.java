c2ae342b5738d558e7217a5b518feaa55b3c947a
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-18 20:52:37 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("mnemonic78@yandex.ru", "Koregin Evgeny Vladimirovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
