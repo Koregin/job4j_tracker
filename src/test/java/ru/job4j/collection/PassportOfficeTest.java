c0c5d7f2eedfb8d3239105b222c730e166e5c486
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-19 10:15:28 +0300

    1. Паспорт и жители [#212656 #217770]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -22,6 +22,6 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         boolean expected = office.add(citizen2);
-        assertThat(expected, is(false));
+        assertFalse(expected);
     }
 }
