da78d8637dff8e8e958f0005e135b52044ba3f54
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-21 12:02:54 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -1,10 +1,11 @@
 package ru.job4j.tracker;
 
-import org.junit.Assert;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class ItemTest {
 
@@ -25,7 +26,7 @@ public class ItemTest {
                 new Item(5, "test5")
         );
         Collections.sort(notSortedItems);
-        Assert.assertEquals(notSortedItems, sortedItems);
+        assertEquals(notSortedItems, sortedItems);
     }
 
     @Test
@@ -45,6 +46,6 @@ public class ItemTest {
                 new Item(1, "test1")
         );
         Collections.sort(notSortedItems, new SortByIdItemReverse());
-        Assert.assertEquals(notSortedItems, sortedItems);
+        assertEquals(notSortedItems, sortedItems);
     }
 }
