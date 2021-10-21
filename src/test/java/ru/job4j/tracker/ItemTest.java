e6ac59c9458e6f50a41f09987206ef6f7e21045e
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-21 11:41:37 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ItemTest {
+
+    @Test
+    public void sortItemTest() {
+        List<Item> notSortedItems = Arrays.asList(
+                new Item(3, "test3"),
+                new Item(1, "test1"),
+                new Item(2, "test2"),
+                new Item(5, "test5"),
+                new Item(4, "test4")
+        );
+        List<Item> sortedItems = Arrays.asList(
+                new Item(1, "test1"),
+                new Item(2, "test2"),
+                new Item(3, "test3"),
+                new Item(4, "test4"),
+                new Item(5, "test5")
+        );
+        Collections.sort(notSortedItems);
+        Assert.assertEquals(notSortedItems, sortedItems);
+    }
+
+    @Test
+    public void sortItemReversedTest() {
+        List<Item> notSortedItems = Arrays.asList(
+                new Item(3, "test3"),
+                new Item(1, "test1"),
+                new Item(2, "test2"),
+                new Item(5, "test5"),
+                new Item(4, "test4")
+        );
+        List<Item> sortedItems = Arrays.asList(
+                new Item(5, "test5"),
+                new Item(4, "test4"),
+                new Item(3, "test3"),
+                new Item(2, "test2"),
+                new Item(1, "test1")
+        );
+        Collections.sort(notSortedItems, new SortByIdItemReverse());
+        Assert.assertEquals(notSortedItems, sortedItems);
+    }
+}
