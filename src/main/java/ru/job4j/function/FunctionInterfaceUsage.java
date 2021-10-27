0374c2f962a4154d8976bbbc87a2a996aa44e469
psyhoenergy <psyhoenergy@gmail.com>
2021-10-27 22:54:53 +0300

    1.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
+++ src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
@@ -3,13 +3,12 @@ package ru.job4j.function;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class FunctionInterfaceUsage {
     public static void main(String[] args) {
         Supplier<String> sup = () -> "New String For Interface";
+        System.out.println(sup.get());
         Consumer<String> consumer = (s) -> System.out.println(s);
         consumer.accept(sup.get());
         BiConsumer<String, String> biconsumer = (s, s1) -> System.out.println(s + s1);
@@ -23,5 +22,21 @@ public class FunctionInterfaceUsage {
         for (String s : strings) {
             stringBiConsumer.accept(i++, " is " + s);
         }
+
+        Predicate<String> pred = s -> s.isEmpty();
+        System.out.println("String is empty: " + pred.test(""));
+        System.out.println("String is empty: " + pred.test("test"));
+
+        UnaryOperator<StringBuilder> builder = b -> b.reverse();
+        System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("String for test")));
+        System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("tset rof gnirtS")));
+
+        BinaryOperator<StringBuilder> builder2 = (b1, b2) -> b1.append(" ").append(b2);
+        System.out.println(
+                "Строка после объединения: " + builder2.apply(
+                        new StringBuilder("First string"),
+                        new StringBuilder("Second string")
+                )
+        );
     }
 }
