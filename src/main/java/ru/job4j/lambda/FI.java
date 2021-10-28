4ac59d0bb9ba1869289564e2b292691a7ed0e6cf
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-28 17:44:20 +0300

    1.3. Встроенные функциональные интерфейсы. [#249209]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -10,12 +10,7 @@ public class FI {
                 new Attachment("image 3", 120),
                 new Attachment("image 2", 23)
         };
-        //Stream<Attachment> stream = Arrays.stream(atts);
-        //stream.forEach(System.out::println);
         Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
-        //Arrays.sort(atts, comparator);
-        //Stream<Attachment> stream2 = Arrays.stream(atts);
-        //stream2.forEach(System.out::println);
         String[] strings = new String[]{"big", "and", "eyes", "circle", "dimension"};
         System.out.println("Before sort: " + Arrays.toString(strings));
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
