539e83a497e3882d2598f17658b608949608b1ce
Evgeny Koregin <mnemonic78@yandex.ru>
2021-11-02 14:31:53 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class ProfilesTest {
     @Test
     public void whenCollectAddresses() {
@@ -20,13 +22,36 @@ public class ProfilesTest {
                 "Angelstreet", 355, 28)));
         List<Address> expected = new ArrayList<Address>();
         expected.add(new Address("Albuquerque", "Mainstreet", 10, 123));
-        expected.add(new Address("New York", "Wallstreet", 5, 355));
-        expected.add(new Address("Washington", "Linkoln", 55, 5));
         expected.add(new Address("Las Vegas", "Casinostreet", 25, 17));
         expected.add(new Address("Los Angeles", "Angelstreet", 355, 28));
-        assertThat(expected, Profiles.collect(profiles));
+        expected.add(new Address("New York", "Wallstreet", 5, 355));
+        expected.add(new Address("Washington", "Linkoln", 55, 5));
+        assertEquals(expected, Profiles.collect(profiles));
     }
 
-    private void assertThat(List<Address> expected, List<Address> collect) {
+    @Test
+    public void whenAddressesWithDuplicates() {
+        List<Profile> profiles = new ArrayList<Profile>();
+        profiles.add(new Profile(new Address("Albuquerque",
+                "Mainstreet", 10, 123)));
+        profiles.add(new Profile(new Address("New York",
+                "Wallstreet", 5, 355)));
+        profiles.add(new Profile(new Address("Washington",
+                "Linkoln", 55, 5)));
+        profiles.add(new Profile(new Address("Las Vegas",
+                "Casinostreet", 25, 17)));
+        profiles.add(new Profile(new Address("Las Vegas",
+                "Casinostreet", 25, 17)));
+        profiles.add(new Profile(new Address("Los Angeles",
+                "Angelstreet", 355, 28)));
+        profiles.add(new Profile(new Address("Los Angeles",
+                "Angelstreet", 355, 28)));
+        List<Address> expected = new ArrayList<Address>();
+        expected.add(new Address("Albuquerque", "Mainstreet", 10, 123));
+        expected.add(new Address("Las Vegas", "Casinostreet", 25, 17));
+        expected.add(new Address("Los Angeles", "Angelstreet", 355, 28));
+        expected.add(new Address("New York", "Wallstreet", 5, 355));
+        expected.add(new Address("Washington", "Linkoln", 55, 5));
+        assertEquals(expected, Profiles.collect(profiles));
     }
 }
